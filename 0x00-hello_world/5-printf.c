6f0f65c4293084b3e92c1572513ee03ffc476208
wizetoons <toritsejuwisdom@gmail.com>
2022-06-30 20:56:44 -0700

    commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
-
 /**
- *  * main - Entry point
- *   * print to output
- *    * Return: Always 0 (Success)
- *     */
-
+ *main - Entry point
+ *print to output
+ *Return: Always 0 (Success)
+ */
 int main(void)
 {
 		printf("with proper grammar, but the outcome is a piece of art,\n");
