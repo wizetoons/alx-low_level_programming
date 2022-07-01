7ce43cb02de3a4a58ef23e45be687b7846fce56c
wizetoons <toritsejuwisdom@gmail.com>
2022-06-30 20:52:23 -0700

    commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
-
 /**
- *  * main - Entry point
- *   * print word to output
- *    * Return: Always 0 (Success)
- *     */
-
+ *main - Entry point
+ *print word to output
+ *Return: Always 0 (Success)
+ */
 int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
