ac108d60b6caa51dc32a7111bcbfa0b3c0af1c70
wizetoons <toritsejuwisdom@gmail.com>
2022-07-06 12:50:16 -0700

    commit

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,50 +1,50 @@
-/*
- *File: 100-times_table.c
- *Auth: Dr Marcus Imagwe
- */
 #include "main.h"
 /**
- *print_times_table - prints times table
- *@n : times table to use
- *Return:void
+ *print_times_table - prints the n times table, starting with 0
+ *@n: number of the times table
  */
 void print_times_table(int n)
 {
-int a = 0, rep, b;
-if (n < 0 || n > 15)
-return;
-while (a <= n)
+int i, j, k;
+if (n >= 0 && n <= 15)
 {
-for (b = 0; b <= n; b++)
+for (i = 0; i <= n; i++)
 {
-rep = a * b;
-if (b == 0)
-_putchar('0' + rep);
-else if (rep < 10)
+for (j = 0; j <= n; j++)
 {
-_putchar(' ');
-_putchar(' ');
-_putchar('0' + rep);
-}
-else if (rep < 100)
+k = j * i;
+if (j == 0)
 {
-_putchar(' ');
-_putchar('0' + rep / 10);
-_putchar('0' + rep % 10)
+_putchar(k + '0');
 }
 else
+if (k < 10 && j != 0)
 {
-_putchar('0' + rep / 100);
-_putchar('0' + (rep - 100) / 10);
-_putchar('0' + rep % 10);
+_putchar(',');									_putchar(' ');
+_putchar(' ');
+_putchar(' ');
+_putchar(k + '0');
 }
-if (b < n)
+else
+if (k >= 10 && k < 100)
 {
 _putchar(',');
 _putchar(' ');
+_putchar(' ');
+_putchar((k / 10) + '0');
+_putchar((k % 10) + '0');
+}
+else
+if (k >= 100)
+{
+_putchar(',');
+_putchar(' ');
+_putchar((k / 100) + '0');
+_putchar(((k / 10) % 10) + '0');
+_putchar((k % 10) + '0');
 }
 }
 _putchar('\n');
-a++;
+}
 }
 }
