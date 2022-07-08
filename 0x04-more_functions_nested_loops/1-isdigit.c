307ad44c246358ba75ed2abcac5afe580ef2963b
wizetoons <toritsejuwisdom@gmail.com>
2022-07-07 17:10:23 -0700

    commit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,12 +1,21 @@
 #include "main.h"
 
-/**
- * * _isdigit - funtion to check for a digit
- * * @c: an integer argument
- * * Return: success
- */
 
-int _isdigit(int c)
+
+/**
+ *
+ * * _isupper -> this is a function to check upper case alphas
+ *
+ * * @c: an integer paramer
+ *
+ * * Return: something but success
+ *
+ * */
+
+int _isupper(int c)
+
 {
-	return (c >= '0' && c <= '9');
+
+		return (c >= 'A' && c <= 'Z');
+
 }
