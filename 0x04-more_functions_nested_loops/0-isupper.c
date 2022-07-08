cda48952b20f20bc28238141d2c0c1b37e9da2df
wizetoons <toritsejuwisdom@gmail.com>
2022-07-08 04:15:37 -0700

    commit

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,17 +1,11 @@
 #include "main.h"
 
 /**
- *_isupper - uppercase letters
- *@c: character to check
- *
- * Return: 0 or 1
+ *_isupper -> this is a function to check upper case alphas
+ *@c: an integer paramer
+ *Return: something but success
  */
 int _isupper(int c)
-
 {
-	if (c >= 'A' && c <= 'Z')
-		return (1);
-	else
-		return (0);
-
+		return (c >= 'A' && c <= 'Z');
 }
