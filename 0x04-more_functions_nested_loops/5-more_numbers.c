375c5a30498cf0a9d9c6ce6c001da89312b0d545
wizetoons <toritsejuwisdom@gmail.com>
2022-07-11 15:51:23 -0700

    commit

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -5,16 +5,17 @@
  */
 void more_numbers(void)
 {
-		int x, y;
+	int x, y;
 
-		for (x = 0; x < 10; x++)
-		{
+	for (x = 0; x < 10; x++)
+	{
 		for (y = 0; y < 15; y++)
 		{
-		if (y >= 10)
-			_putchar((y / 10) + '0');
+			if (y >= 10)
+				_putchar((y / 10) + '0');
 			_putchar((y % 10) + '0');
 		}
-			_putchar('\n');
-		}
+		_putchar('\n');
+
+											}
 }
