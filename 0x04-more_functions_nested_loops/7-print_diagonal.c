01647000a3ba4c556a1b0a61c5d871766f23a5d7
wizetoons <toritsejuwisdom@gmail.com>
2022-07-11 16:13:07 -0700

    commit

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -6,17 +6,18 @@
  */
 void print_diagonal(int n)
 {
-		int x, i;
+	int x, i;
 
-			if (n <= 0)
-				_putchar('\n');
-			else
-			{
-				for (x = 0; x < n; x++)
-			{
-				for (i = 0; i < x; i++)
-					_putchar(' ');
-					_putchar(92);
-					_putchar('\n');					}
-			}
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (x = 0; x < n; x++)
+		{
+			for (i = 0; i < x; i++)
+				_putchar(' ');
+			_putchar(92);
+			_putchar('\n');
+		}
+	}
 }
