4eeb98d0ec42ac922449d7559618bafe97bdd0c7
wizetoons <toritsejuwisdom@gmail.com>
2022-07-22 18:09:18 -0700

    commit

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - straing point
+ * @argc: argument counter
+ * @argv: array pointer to the strings
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[] __attribute__ ((unused)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
