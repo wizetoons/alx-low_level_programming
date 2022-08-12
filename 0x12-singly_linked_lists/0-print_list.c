11bb010f4575f9707c5067e846f204a6acf545f4
wizetoons <toritsejuwisdom@gmail.com>
2022-08-11 19:55:44 -0700

    task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -4,18 +4,20 @@
 #include "lists.h"
 
 /**
- *print_list - check the code for Holberton School students. 
+ * print_list - check the code for Holberton School students.
  * @h: name of the list
  * Return: the number of nodes.
  */
 size_t print_list(const list_t *h)
 {
-	int count = 0; while (h)
+	int count = 0;
+
+	while (h)
 	{
 		if (h->str == NULL)
-	{
-		printf("[0] (nil)\n");
-	}
+		{
+			printf("[0] (nil)\n");
+		}
 		else
 		{
 			printf("[%d] %s\n", h->len, h->str);
